0b4baf841a24c8db397c1ddc7aa5432a34df2d5b
stevecd <stevecd123@gmail.com>
2016-02-13 21:34:14 -0500

    Adding most basic deploy command

---
+++ webpack.production.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+var node_modules_dir = path.resolve(__dirname, 'node_modules')
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src'),
+        exclude: [node_modules_dir]
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+    ]
+  }
+};
